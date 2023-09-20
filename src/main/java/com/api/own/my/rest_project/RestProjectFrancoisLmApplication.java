ac708701964f05cbff1c20b3211c0393bba0739e
flemerrer <francois.lemerrer@pm.me>
2023-09-20 08:42:14 +0200

    setup

---
+++ src/main/java/com/api/own/my/rest_project/RestProjectFrancoisLmApplication.java
@@ -0,0 +1,13 @@
+package com.api.own.my.rest_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProjectFrancoisLmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestProjectFrancoisLmApplication.class, args);
+    }
+
+}
